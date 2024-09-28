fb2aa7254147e66983ff572d4c0ed819e6aa80e8
stskri <177446189+stskri@users.noreply.github.com>
2024-09-28 21:59:51 +0000

    [finish]notification

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,14 +2,21 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+  has_many :notifications, as: :notifiable, dependent: :destroy
+
+  after_create do
+    user.followers.each do |follower|
+      notifications.create(user_id: follower.id)
+    end
+  end
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-	
+
   def self.search_for(content, method)
     if method == 'perfect'
       Book.where(title: content)
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,4 +2,10 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :book
   validates_uniqueness_of :book_id, scope: :user_id
+  has_one :notification, as: :notifiable, dependent: :destroy
+  
+  after_create do
+    create_notification(user_id: book.user_id)
+  end
+  
 end
